c37f51f6650c1e44508c2497fd010bcac589b502
Your Name <you@example.com>
2018-11-27 17:33:20 -0500

    c

--- discador/static/app.js
+++ discador/static/app.js
@@ -1,125 +1,164 @@
 
 
-class Prueba extends React.Component{
-  constructor(){
-     super();
-     this.state= {title: 'Click text'}
-  }
-  
-  render(){
-     return <div onClick= {() => this.setState({title:'New text'})}>{this.state.title}</div>
-  } 
-}
-
-ReactDOM.render(<Prueba/>, document.getElementById('carteras'))
+            class Hello extends React.Component {
+                
+              constructor(props) {
+                  super(props);
 
 
-class Agentes extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-
-    this.state= {title: 'Click text'}
-   
-  }
-
-  componentDidMount() {
-    fetch("http://138.68.230.137:8000/distrito")
-      .then(res => res.json())
-      .then(
-        (result) => {
-
-          console.log(result)
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+                  this.state = {
+                      error: null,
+                      isLoaded: false,
+                      proveedores: [],
+                      carteras:[],
+                      resultados:[]
+                  };
 
 
-
-  sacacarteras(e) {
-
-      fetch("http://138.68.230.137:8000/distrito")
-      .then(res => res.json())
-      .then(
-        (result) => {
-
-          console.log(result)
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-
-  }
-
-
-  render() {
-    const { error, isLoaded, items } = this.state;
+                  console.log(this.state)
 
  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li onClick={(e) => this.sacacarteras(item, e)} key={item.nombre} >
-              {item.nombre}
-            </li>
-          ))}
-        </ul>
-      );
-    }
-  }
+              
+              }
+
+
+
+              componentDidMount() {
+
+                     fetch("http://localhost:8000/discador/api_proveedor/")
+                          .then(res => res.json())
+                          .then(
+                              (result) => {
+
+                              
+                              this.setState({
+                                  isLoaded: true,
+                                  proveedores: result
+                              });
+
+                            
+                              },
+
+                              (error) => {
+                             
+                              }
+                          )
+               
+
+              }
+
+
+     
+
+
+
+              sacacarteras(item) {
+                  
+                  fetch("http://localhost:8000/discador/api_cartera/"+item.id)
+                  .then(res => res.json())
+                  .then(
+                      (result) => {
+
+                      console.log(result)
+                      this.setState({
+                          isLoaded: true,
+                          carteras: result
+                      });
+                      },
+                      // Note: it's important to handle errors here
+                      // instead of a catch() block so that we don't swallow
+                      // exceptions from actual bugs in components.
+                      (error) => {
+                      this.setState({
+                          isLoaded: true,
+                          error
+                      });
+                      }
+                  )
+
+              }
+
+
+              sacaresultados(item) {
+                  
+                  fetch("http://localhost:8000/discador/api_cartera/"+item.id)
+                  .then(res => res.json())
+                  .then(
+                      (result) => {
+
+                      console.log(result)
+                      this.setState({
+                          isLoaded: true,
+                          carteras: result
+                      });
+                      },
+                      // Note: it's important to handle errors here
+                      // instead of a catch() block so that we don't swallow
+                      // exceptions from actual bugs in components.
+                      (error) => {
+                      this.setState({
+                          isLoaded: true,
+                          error
+                      });
+                      }
+                  )
+
+              }
+
+              render() {
+
+
+                  const { error, isLoaded, proveedores, carteras } = this.state;
+
+                      if (!isLoaded) {
+                          
+                          return <div>Loading...</div>;
+                          
+                      } 
+                      
+                      else {
+                          
+                          return (
+                              <ul>
+                              <h1>Proveedores</h1>
+
+
+                              {proveedores.map(item => (
+
+                                  <li onClick={(e) => this.sacacarteras(item, e)} key={item.nombre} >
+                                  {item.nombre}
+                                  </li>
+
+                              ))}
+
+                              <h1>Carteras</h1>
+
+                              {carteras.map(item => (
+                              <li onClick={(e) => this.sacaresultados(item, e)} key={item.nombre} >
+                              {item.nombre}
+                              </li>
+                              ))}
+
+                              
+                              
+                              </ul>
+                          );
+                          }
+              }
+
+
+          }
 
 
 
 
 
-
-}
-
+          ReactDOM.render(
 
 
+              <Hello />,
+              document.getElementById('root')
 
 
-
-ReactDOM.render(
-    <Agentes />,
-    document.getElementById('root')
-);
-
-ReactDOM.render(
-    <Title />,
-    document.getElementById('carteras')
-);
+          );
 
---
+++ discador/static/bundle.js
@@ -0,0 +1,157 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Hello = function (_React$Component) {
+    _inherits(Hello, _React$Component);
+
+    function Hello(props) {
+        _classCallCheck(this, Hello);
+
+        var _this = _possibleConstructorReturn(this, (Hello.__proto__ || Object.getPrototypeOf(Hello)).call(this, props));
+
+        _this.state = {
+            error: null,
+            isLoaded: false,
+            proveedores: [],
+            carteras: [],
+            resultados: []
+        };
+
+        console.log(_this.state);
+
+        return _this;
+    }
+
+    _createClass(Hello, [{
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            var _this2 = this;
+
+            fetch("http://localhost:8000/discador/api_proveedor/").then(function (res) {
+                return res.json();
+            }).then(function (result) {
+
+                _this2.setState({
+                    isLoaded: true,
+                    proveedores: result
+                });
+            }, function (error) {});
+        }
+    }, {
+        key: "sacacarteras",
+        value: function sacacarteras(item) {
+            var _this3 = this;
+
+            fetch("http://localhost:8000/discador/api_cartera/" + item.id).then(function (res) {
+                return res.json();
+            }).then(function (result) {
+
+                console.log(result);
+                _this3.setState({
+                    isLoaded: true,
+                    carteras: result
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            function (error) {
+                _this3.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            });
+        }
+    }, {
+        key: "sacaresultados",
+        value: function sacaresultados(item) {
+            var _this4 = this;
+
+            fetch("http://localhost:8000/discador/api_cartera/" + item.id).then(function (res) {
+                return res.json();
+            }).then(function (result) {
+
+                console.log(result);
+                _this4.setState({
+                    isLoaded: true,
+                    carteras: result
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            function (error) {
+                _this4.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var _this5 = this;
+
+            var _state = this.state,
+                error = _state.error,
+                isLoaded = _state.isLoaded,
+                proveedores = _state.proveedores,
+                carteras = _state.carteras;
+
+
+            if (!isLoaded) {
+
+                return React.createElement(
+                    "div",
+                    null,
+                    "Loading..."
+                );
+            } else {
+
+                return React.createElement(
+                    "ul",
+                    null,
+                    React.createElement(
+                        "h1",
+                        null,
+                        "Proveedores"
+                    ),
+                    proveedores.map(function (item) {
+                        return React.createElement(
+                            "li",
+                            { onClick: function onClick(e) {
+                                    return _this5.sacacarteras(item, e);
+                                }, key: item.nombre },
+                            item.nombre
+                        );
+                    }),
+                    React.createElement(
+                        "h1",
+                        null,
+                        "Carteras"
+                    ),
+                    carteras.map(function (item) {
+                        return React.createElement(
+                            "li",
+                            { onClick: function onClick(e) {
+                                    return _this5.sacaresultados(item, e);
+                                }, key: item.nombre },
+                            item.nombre
+                        );
+                    })
+                );
+            }
+        }
+    }]);
+
+    return Hello;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Hello, null), document.getElementById('root'));
